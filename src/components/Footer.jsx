e7042ee304f36124b1240224b227840831a25c5c
mcattani <marianocattani@gmail.com>
2025-11-30 15:41:30 -0300

    actualización de Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap";
+import { FaBlogger } from "react-icons/fa";
 
 export default function Footer() {
 
@@ -6,8 +7,26 @@ export default function Footer() {
         <footer className="bg-dark text-white text-center py-3 mt-5">
             <Container>
                 <hr />
-                <p className="mb-0">2025 🄯 The Nerdy Apprentice - Todos los errores reservados.</p>
+                <p className="mb-0">
+                    2025 🄯 - {" "}
+                    <a
+                        href="https://thenerdyapprentice.blogspot.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-white"
+                        style={{ textDecoration: "none" }}
+                    >                       
+                        The Nerdy Apprentice
+                    </a>{" "}<FaBlogger
+                            size={18}
+                            style={{
+                                marginRight: "6px",
+                                verticalAlign: "middle",
+                            }}
+                        />
+                    - Todos los errores reservados.
+                </p>
             </Container>
         </footer>
-    )
+    );
 }
